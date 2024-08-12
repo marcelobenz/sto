5acf514fe797cbab9aa89470e9a5685690338416
Juan Ignacio Leiva <jleiva@cognisys.com.ar>
2024-08-12 16:03:45 -0300

    Update Login: se implementa redirect a Oauth y obtencion de token

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TramiteController;
 use App\Http\Controllers\DashboardController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 
 /*
@@ -16,8 +18,32 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+Route::get('/success', function (Request $request) {
+    /* $state = $request->session()->pull('state');
+ 
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class,
+        'Invalid state value.'
+    ); */
+
+    $code = $request->code;
+
+    $response = Http::asForm()->post('http://localhost:8000/oauth/token', [
+        'grant_type' => 'authorization_code',
+        'client_id' => '1',
+        'client_secret' => 'GToG3ZMH8toPdQsJ3Jq0YSnBBZp4xRUbqcBl855Z',
+        'redirect_uri' => 'http://127.0.0.1:8001/success',
+        'code' => $request->code,
+    ]);
+
+    $res = $response->json();
+
+    return $response->json();
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('http://localhost:8000/oauth/authorize?client_id=1&redirect_uri=http://127.0.0.1:8001/success&response_type=code&scope=&state=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf');
 });
 
 Route::get('/navbar', function () {
