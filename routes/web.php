9ff0e74c9dde910786564737b11eaaa9910c450a
Juan Ignacio Leiva <juanileiva17@gmail.com>
2024-08-13 15:11:02 -0300

    Revert "Update Login: se implementa redirect a Oauth y obtencion de token"
    This reverts commit 5acf514fe797cbab9aa89470e9a5685690338416.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TramiteController;
 use App\Http\Controllers\DashboardController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 
 /*
@@ -18,32 +16,8 @@ use Illuminate\Support\Facades\Http;
 |
 */
 
-Route::get('/success', function (Request $request) {
-    /* $state = $request->session()->pull('state');
- 
-    throw_unless(
-        strlen($state) > 0 && $state === $request->state,
-        InvalidArgumentException::class,
-        'Invalid state value.'
-    ); */
-
-    $code = $request->code;
-
-    $response = Http::asForm()->post('http://localhost:8000/oauth/token', [
-        'grant_type' => 'authorization_code',
-        'client_id' => '1',
-        'client_secret' => 'GToG3ZMH8toPdQsJ3Jq0YSnBBZp4xRUbqcBl855Z',
-        'redirect_uri' => 'http://127.0.0.1:8001/success',
-        'code' => $request->code,
-    ]);
-
-    $res = $response->json();
-
-    return $response->json();
-});
-
 Route::get('/', function () {
-    return redirect('http://localhost:8000/oauth/authorize?client_id=1&redirect_uri=http://127.0.0.1:8001/success&response_type=code&scope=&state=asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf');
+    return view('welcome');
 });
 
 Route::get('/navbar', function () {
