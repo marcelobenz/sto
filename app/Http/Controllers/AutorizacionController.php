f4a414ace6d3b4a415ca865f413cf6b5e4697a86
Juan Ignacio Leiva <jleiva@cognisys.com.ar>
2024-08-21 15:52:45 -0300

    Update AutorizacionController: integración con API usuarios de CVSA y refresh de token finalizadas

--- app/Http/Controllers/AutorizacionController.php
+++ app/Http/Controllers/AutorizacionController.php
@@ -5,26 +5,57 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
+use App\Models\UsuarioView;
+use App\Models\RequestLoginEmpleado;
+use App\Models\IngresanteBuilder;
+use App\Models\Ingresante;
 
 class AutorizacionController extends Controller
 {
     public function index(Request $request)
     {
-        $this->obtenerUsuario();
-        //retorna de obtenerUsuario()
-        //Se obtiene usuario de base, si es incorrecto se lanza excepcion "El usuario ingresado es incorrecto" y si no esta activo
-        //"El usuario ingresado no esta activo"
-        //Si es correcto retorna y se redirecciona a "bandeja_personal"
+        try {
+            $user = $this->obtenerUsuario();
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 
-    private function obtenerUsuario() {
-        if(Session::get('EXPIRES_IN') !== 0) {
-            $this->obtenerTokenTrasSesionExpirada();
-        }
+    private function obtenerUsuario(): UsuarioView {
+        try {
+            if(Session::get('EXPIRES_IN') !== 0) {
+                $this->obtenerTokenTrasSesionExpirada();
+            }
+    
+            //Llamado servicio usuario CVSA
+            Http::asForm()->post(env('SERVICIO_TOKEN_CVSA'), [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => Session::get('REFRESH_TOKEN'),
+                'client_id' => env('OAUTH_ID'),
+                'client_secret' => env('OAUTH_SECRET'),
+                'scope' => '',
+            ]);
+    
+            $access_token = Session::get('ACCESS_TOKEN');
+    
+            $response = Http::withHeaders([
+                'Authorization' => "Bearer {$access_token}"
+            ])->get(env('SERVICIO_USUARIO_CVSA')); 
+    
+            $res = $response->json();
+    
+            if($res['cuil'] === null || $res['cuil'] === "") {
+                //throw new RuntimeAlert("Faltan datos del usuario");
+            }
+    
+            $requestView = new RequestLoginEmpleado();
+		    $requestView->setUsuario(new UsuarioView($res['apellido'], "?", "N", "?", "?", "?", $res['cuil'], $res['email'], $res['name'], "?", "?"));
 
-        //Llamado servicio usuario CVSA
-        //Validaciones, getUsuario, excepciones "Faltan datos del usuario" y "No es posible recuperar el usuario"
-        //return
+		    return $requestView->getUsuario();
+        } catch (\Throwable $th) {
+            //throw new ExcepcionControladaError("No es posible recuperar el usuario", e);
+            error_log($th);
+        }
     }
 
     private function obtenerTokenTrasSesionExpirada() {
