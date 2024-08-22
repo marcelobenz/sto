d301fba929a0342248f4c24b5afba90193861db4
Juan Ignacio Leiva <jleiva@cognisys.com.ar>
2024-08-22 18:15:14 -0300

    Update AutorizacionController -> logica de obtener usuario y redirect a Bandeja Personal + control excepcion

--- app/Http/Controllers/AutorizacionController.php
+++ app/Http/Controllers/AutorizacionController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
-use App\Models\UsuarioView;
-use App\Models\RequestLoginEmpleado;
+use Illuminate\Support\Facades\Redirect;
 use App\Models\IngresanteBuilder;
 use App\Models\Ingresante;
 
@@ -16,9 +15,49 @@ class AutorizacionController extends Controller
     {
         try {
             $user = $this->obtenerUsuario();
+            $ingresanteBuilder = new IngresanteBuilder();
+            $ingresanteBuilder->setCuil($user->getCuil());
+            $ingresante = $ingresanteBuilder->build();
+            $ingresante->ingresar();
+            return Redirect::to('/bandeja-personal');
         } catch (\Throwable $th) {
-            //throw $th;
+            throw new Exception("Error al recuperar el usuario de base", $th);
+
+            //Actualizar codigo de acuerdo al catch comentado de abajo
         }
+
+        /* 
+        } catch (NullPointerException e) { 
+        	try {
+            	UsuarioInterno usuario = new UsuarioInterno();
+                usuario.setLegajo(this.obtenerUsuario().getCuil());
+				usuario.setCuit(this.obtenerUsuario().getCuil());
+				usuario.setDni(this.obtenerUsuario().getCuil().substring(2, (this.obtenerUsuario().getCuil().length() - 1)));
+	            usuario.setCorreoMunicipal(this.obtenerUsuario().getMail());
+	            usuario.setNombre(this.obtenerUsuario().getNombre());
+	            usuario.setApellido(this.obtenerUsuario().getApellido());
+	            
+	            OficinaInterna oficina = new OficinaInterna(1, "001", "EXPLOTACIONES");
+	            usuario.setOficina(oficina.getNombre());
+	        	
+	        	new UsuarioInternoController().crearUsuarioDefault(usuario, oficina);
+	        	
+	        	new AsignableATramiteController().retieveAllFromDB();
+	        	
+	        	Ingresante ingresante = new IngresanteBuilder().setCuil(this.obtenerUsuario().getCuil()).build();
+	            ingresante.ingresar();
+	            this.setAnalizando(false);
+	            this.redireccionar(Navegacion.bandeja_personal);
+			} catch (Exception e1) {
+				this.setMensaje(e.getMessage());
+	            VTULogger.getLogger().error(e.getMessage(), e);
+			}
+    	} catch (RuntimeAlert e) {
+            this.setMensaje(e.getMessage());
+        } catch (Exception e) {
+            this.setMensaje(e.getMessage());
+            VTULogger.getLogger().error(e.getMessage(), e);
+        } */
     }
 
     private function obtenerUsuario(): UsuarioView {
@@ -45,7 +84,7 @@ class AutorizacionController extends Controller
             $res = $response->json();
     
             if($res['cuil'] === null || $res['cuil'] === "") {
-                //throw new RuntimeAlert("Faltan datos del usuario");
+                throw new Exception("Faltan datos del usuario");
             }
     
             $requestView = new RequestLoginEmpleado();
@@ -53,8 +92,7 @@ class AutorizacionController extends Controller
 
 		    return $requestView->getUsuario();
         } catch (\Throwable $th) {
-            //throw new ExcepcionControladaError("No es posible recuperar el usuario", e);
-            error_log($th);
+            throw new ExcepcionControladaError("No es posible recuperar el usuario", $th);
         }
     }
 
