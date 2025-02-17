66a93c6cdee24c2e62d03e0d8595b6782ff926ce
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-02-17 16:53:18 -0300

    Update Multinotas -> Se optimizo logica de carga de datos con cacheo y verificacion de ultimo update

--- app/Http/Controllers/MultinotaController.php
+++ app/Http/Controllers/MultinotaController.php
@@ -6,54 +6,84 @@ use stdClass;
 use App\Models\TipoTramiteMultinota;
 use App\Models\Categoria;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Yajra\DataTables\DataTables;
+use Carbon\Carbon;
 use DB;
 
 class MultinotaController extends Controller
 {
     public function index(Request $request) {
+        $shouldRefreshData = false;
         $dataFiltrada = null;
 
-        $data = TipoTramiteMultinota::distinct()
-            ->where('baja_logica', 0)
-            ->orderBy('nombre')
-            ->get();
+        // Chequea si la data existe en la cache
+        if (!Cache::has('DATA_MULTINOTAS') || !Cache::has('CATEGORIAS') || !Cache::has('LAST_UPDATE_MULTINOTAS')) {
+            $shouldRefreshData = true; // No existe asi que tenemos que refrescarla
+        } else {
+            // Chequea si la data necesita ser recuperada de base de nuevo segun la fecha de ultima actualizacion
+            $lastUpdate = Cache::get('LAST_UPDATE_MULTINOTAS');
+            $latestMultinotaUpdate = TipoTramiteMultinota::max('fecha_ultima_actualizacion');
+            // Se convierte la fecha de ultima actualización de base a una instancia de la librería Carbon
+            $latestMultinotaUpdateUTC = Carbon::createFromFormat('Y-m-d H:i:s', $latestMultinotaUpdate, 'UTC');
 
-        $categorias = Categoria::where('flag_activo', 1)
-            ->orderBy('nombre')
-            ->get();
+            // Se convierte la instancia a GMT-3
+            $latestMultinotaUpdateHoraLocal = $latestMultinotaUpdateUTC->setTimezone('America/Sao_Paulo');
 
-        foreach ($data as $d) {
-            //Se parsea la fecha de alta
-            $fechaAltaParseObject = date_parse($d->fecha_alta);
-            if($fechaAltaParseObject['month'] >= 1 && $fechaAltaParseObject['month'] <= 9) {
-                if($fechaAltaParseObject['day'] >= 1 && $fechaAltaParseObject['day'] <= 9) {
-                    $d->fecha_alta = "0{$fechaAltaParseObject['day']}-0{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+            if ($latestMultinotaUpdateHoraLocal > $lastUpdate) {
+                $shouldRefreshData = true; // La base fue actualizada, necesitamos refrescar la cache
+            }
+        }
+
+        if ($shouldRefreshData) {
+            // Recupera la data desde base
+            $data = TipoTramiteMultinota::distinct()
+                ->where('baja_logica', 0)
+                ->orderBy('nombre')
+                ->get();
+
+            $categorias = Categoria::where('flag_activo', 1)
+                ->orderBy('nombre')
+                ->get();
+
+            // Se formatea la fecha y se recuperan los nombres de las categorias
+            foreach ($data as $d) {
+                $fechaAltaParseObject = date_parse($d->fecha_alta);
+                if ($fechaAltaParseObject['month'] >= 1 && $fechaAltaParseObject['month'] <= 9) {
+                    if ($fechaAltaParseObject['day'] >= 1 && $fechaAltaParseObject['day'] <= 9) {
+                        $d->fecha_alta = "0{$fechaAltaParseObject['day']}-0{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+                    } else {
+                        $d->fecha_alta = "{$fechaAltaParseObject['day']}-0{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+                    }
                 } else {
-                    $d->fecha_alta = "{$fechaAltaParseObject['day']}-0{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+                    if ($fechaAltaParseObject['day'] >= 1 && $fechaAltaParseObject['day'] <= 9) {
+                        $d->fecha_alta = "0{$fechaAltaParseObject['day']}-{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+                    } else {
+                        $d->fecha_alta = "{$fechaAltaParseObject['day']}-{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+                    }
                 }
-            } else {
-                if($fechaAltaParseObject['day'] >= 1 && $fechaAltaParseObject['day'] <= 9) {
-                    $d->fecha_alta = "0{$fechaAltaParseObject['day']}-{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
-                } else {
-                    $d->fecha_alta = "{$fechaAltaParseObject['day']}-{$fechaAltaParseObject['month']}-{$fechaAltaParseObject['year']}";
+
+                $idCategoria = $d->id_categoria;
+
+                $filteredArray = $categorias->filter(function ($c) use ($idCategoria) {
+                    return $c->id_categoria == $idCategoria;
+                });
+
+                foreach ($filteredArray as $key => $value) {
+                    $d->nombre_categoria = $value->nombre;
                 }
             }
 
-            //Obtener la categoria con el ID
-            $idCategoria = $d->id_categoria;
-
-            $filteredArray = $categorias->filter(function ($c) use ($idCategoria) {
-                // Your filtering logic here
-                return $c->id_categoria == $idCategoria;
-            });
-
-            foreach ($filteredArray as $key => $value) {
-                $d->nombre_categoria = $value->nombre;
-            }
+            // Se guarda la data en cache por 60 minutos
+            Cache::put('DATA_MULTINOTAS', $data, 60 * 60);
+            Cache::put('CATEGORIAS', $categorias, 60 * 60);
+            Cache::put('LAST_UPDATE_MULTINOTAS', now(), 60 * 60);
+        } else {
+            // Se guarda la data desde la cache
+            $data = Cache::get('DATA_MULTINOTAS');
+            $categorias = Cache::get('CATEGORIAS');
         }
 
         if(($request->has('nombre') && !empty($request->nombre))
@@ -61,7 +91,7 @@ class MultinotaController extends Controller
         || $request->has('publicas') && !empty($request->publicas)
         || $request->has('mensajeInicial') && !empty($request->mensajeInicial)
         || $request->has('expediente') && !empty($request->expediente)) {
-            $dataFiltrada = $data;
+            $dataFiltrada = Cache::get('DATA_MULTINOTAS');
 
             if($request->has('nombre') && !empty($request->nombre)) {
                 $nombre = Str::lower($request->nombre);
@@ -127,6 +157,8 @@ class MultinotaController extends Controller
                 ->addIndexColumn()
                 ->make(true);
         }
+
+        $categorias = Cache::get('CATEGORIAS');
         
         return view('multinotas.index', compact('categorias'));
     }
