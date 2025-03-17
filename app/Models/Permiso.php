856cfea270fd2c64db3bc4c024886c995366c3bc
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-03-17 15:50:00 -0300

    Fix nombre de modelo UsuarioPermiso

--- app/Models/Permiso.php
+++ app/Models/Permiso.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class Permiso extends Model
+class UsuarioPermiso extends Model
 {
     use HasFactory;
 
