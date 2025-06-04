ad20ca62fd1a881e10f8bc79a2613be554701c9d
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-06-04 15:00:13 -0300

    feat: informacion adicional - etapa 'resumen multinota'

--- resources/views/partials/etapas-tramite/resumen-multinota.blade.php
+++ resources/views/partials/etapas-tramite/resumen-multinota.blade.php
@@ -1,1 +1,2 @@
 @include('partials.etapas-tramite-resumen.seccion-valor-multinota-resumen', ['formulario' => $formulario])
+@include('partials.etapas-tramite-resumen.informacion-adicional-resumen', ['informacionAdicional' => $informacionAdicional])
