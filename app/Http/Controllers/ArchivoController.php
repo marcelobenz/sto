bff8a935020b39c9d909c822577dfba5687b27cc
Juan Ignacio Leiva <juanileiva17@gmail.com>
2025-06-17 20:56:32 -0300

    fix: archivos, mover archivo a path definitivo

--- app/Http/Controllers/ArchivoController.php
+++ app/Http/Controllers/ArchivoController.php
@@ -94,29 +94,68 @@ class ArchivoController extends Controller
      * Permite subir un archivo adjunto previamente a tener el trámite creado (durante la instanciación).
      */
     public function subirArchivoTemporal(Request $request) {
-        $file = $request->file('archivo');
+        try {    
+            $archivo = $request->file('archivo');
 
-        // Guardar temporalmente
-        $path = $file->store('temp'); // Guarda en storage/app/temp
+            // Generar un nombre único para el archivo (evitar colisiones)
+            $nombreArchivo = time() . '_' . Normalizer::normalize($archivo->getClientOriginalName(), Normalizer::FORM_C);
 
-        // Guarda la metadata en sesión
-        $archivos = Session::get('ARCHIVOS', []);
-        $archivos[] = [
-            'nombre' => $file->getClientOriginalName(),
-            'tipoContenido' => $file->getMimeType(),
-            'pathArchivo' => $path,
-            'peso' => $file->getSize(),
-            'fechaCarga' => now()->format('d-m-Y H:i:s'),
-            'comentario' => '', // Opcional
-        ];
-        Session::put('ARCHIVOS', $archivos);
+            // Guardar temporalmente
+            $path = $archivo->storeAs('temp', $nombreArchivo, 'adjuntos'); // Guarda en storage/app/adjuntos/temp
 
-        $htmlVista = view('partials.etapas-tramite.adjuntar-documentacion', compact('archivos'))->render();
+            // Guarda la metadata en sesión
+            $archivos = Session::get('ARCHIVOS', []);
+            $archivos[] = [
+                'nombre' => $archivo->getClientOriginalName(),
+                'tipoContenido' => $archivo->getMimeType(),
+                'pathArchivo' => $path,
+                'peso' => $archivo->getSize(),
+                'fechaCarga' => now()->format('d-m-Y H:i:s'),
+                'comentario' => '', // Opcional
+            ];
+            Session::put('ARCHIVOS', $archivos);
 
-        return response()->json([
-            'htmlVista' => $htmlVista,
-            'archivos' => $archivos
-        ]);
+            $htmlVista = view('partials.etapas-tramite.adjuntar-documentacion', compact('archivos'))->render();
+
+            return response()->json([
+                'htmlVista' => $htmlVista,
+                'archivos' => $archivos
+            ]);
+        } catch (\Exception $e) {
+            return back()->with('error', 'Error al subir el archivo temporal: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Permite mover un archivo adjunto durante la creación del trámite (temporal) a su directorio definitivo.
+     */
+    public static function moverArchivo($archivosTemp, $idTramite) {
+        try {    
+            // Se crea carpeta para el trámite
+            Storage::disk('adjuntos')->makeDirectory($idTramite);
+
+            foreach ($archivosTemp as &$a) {
+                // Definir la carpeta basada en el ID del trámite
+                $carpetaTramite = $idTramite;
+
+                // Generar un nombre único para el archivo (evitar colisiones)
+                $nombreArchivo = time() . '_' . Normalizer::normalize($a['nombre'], Normalizer::FORM_C);
+
+                // Se define ruta destino
+                $rutaDestino = $carpetaTramite . '/' . $nombreArchivo;
+
+                // Mover archivo de temp a carpeta final
+                Storage::disk('adjuntos')->move($a['pathArchivo'], $rutaDestino);
+
+                //Reasignar path del archivo
+                $a['pathArchivo'] = $rutaDestino;
+            }
+
+            // Se reasignan archivos en sesión
+            Session::put('ARCHIVOS', $archivosTemp);
+        } catch (\Exception $e) {
+            return back()->with('error', 'Error al mover el archivo: ' . $e->getMessage());
+        }
     }
 
     /**
